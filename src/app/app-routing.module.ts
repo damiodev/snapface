5f3f21661e891e499405ec23acbebe2ec5a87052
damiodev <oweb.digital@gmail.com>
2023-06-10 03:34:33 +0200

    Adding the homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'; // On importe la classe NgModule
 import { RouterModule, Routes } from '@angular/router'; // On importe la classe RouterModule et Routes
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component'; // On importe le composant FaceSnapListComponent
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [ // On définit les routes de l'application
     { path: 'facesnaps', component: FaceSnapListComponent }, // Route vers la liste des FaceSnaps
+    { path: '', component: LandingPageComponent } // Route vers la page d'accueil
 ];
 
 @NgModule({ // On décore la classe AppModule avec @NgModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
     FaceSnapComponent,
     FaceSnapListComponent,
     HeaderComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
