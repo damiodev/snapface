a993e7768cebcddf67dfad8068588010fd575b66
damiodev <oweb.digital@gmail.com>
2023-06-10 09:41:23 +0200

    Visualization of each FaceSnap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core'; // On importe la classe NgModule
 import { RouterModule, Routes } from '@angular/router'; // On importe la classe RouterModule et Routes
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component'; // On importe le composant FaceSnapListComponent
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 
 const routes: Routes = [ // On définit les routes de l'application
+    { path: 'facesnaps/:id', component: SingleFaceSnapComponent }, // Route vers la liste des FaceSnaps
     { path: 'facesnaps', component: FaceSnapListComponent }, // Route vers la liste des FaceSnaps
     { path: '', component: LandingPageComponent } // Route vers la page d'accueil
 ];
